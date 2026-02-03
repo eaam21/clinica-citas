e3dba92308d3252fd3ede2635253597e3188a4dd
Elias Alejandro <ealmdz@gmail.com>
2026-02-02 22:53:58 -0500

    kafka: actualizando properties

--- src/main/java/com/clinica/citas/configuration/KafkaConfiguration.java
+++ src/main/java/com/clinica/citas/configuration/KafkaConfiguration.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KafkaConfiguration {
     @Bean
-    public NewTopic paymentTopic() {
+    public NewTopic pacientesTopic() {
         return new NewTopic(Constantes.TOPICO, 1, (short) 1);
     }
 }
