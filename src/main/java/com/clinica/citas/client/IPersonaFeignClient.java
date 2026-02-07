d90d055f93a36403128d60bc6f25217013e2f4cc
Elias Alejandro <ealmdz@gmail.com>
2026-02-07 18:07:00 -0500

    Descrubrimiento de microservicios con Eureka

--- src/main/java/com/clinica/citas/client/IPersonaFeignClient.java
+++ src/main/java/com/clinica/citas/client/IPersonaFeignClient.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
 
-@FeignClient(name="persona", url="localhost:8080")
+//@FeignClient(name="persona", url="localhost:8080")
+@FeignClient(name="personas")
 public interface IPersonaFeignClient {
     @GetMapping("/personas")
     List<PersonaDTO> listarPersonas();
